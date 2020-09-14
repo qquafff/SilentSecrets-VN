96dadd6de840f80352649bdc09c52f8ba5439ea5
Dani <dani.niskanen@student.careeria.fi>
2020-09-14 08:46:56 +0300

    Initial Scripts Commit

---
+++ SilentSecrets--VN/Assets/Scripts/MapSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapSystem : MonoBehaviour
+{
+    public GameObject Map;
+    public string levelName; 
+
+    void mapOpen ()
+    {
+        Map.SetActive(true);
+    }
+
+    void mapClose()
+    {
+        Map.SetActive(false);
+    }
+
+    void SceneLoader()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+
+
+
+
+}
